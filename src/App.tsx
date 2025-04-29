6bfca7b4d09015700ec74f104dab3f9cd430c904
Mickle_PC <mickle.simakov@gmail.com>
2025-04-29 16:21:05 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './css/App.css'
+import {data} from './data'
+import NewsCard from './components/NewsCard/NewsCard'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title'>Hello World</h1>
+      <img src="/favicons/e65d69dc71ab539384fcc63062efdd3d.png" alt=""/>
+    </>
+  )
+}
+
+export default App
