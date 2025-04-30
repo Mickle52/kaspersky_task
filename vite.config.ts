99e655b057dc3c4bd6fd3707f75c8cb30d50ee5b
Mickle_PC <mickle.simakov@gmail.com>
2025-04-30 14:34:29 +0300

    Little fix 0.2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kaspersky-task/',
+  base: '/kaspersky_task/',
   plugins: [react()],
 })
