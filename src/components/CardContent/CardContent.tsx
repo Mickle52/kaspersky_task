2ceab2d1b1f9cbf0414ba190b9e9d0c00998fda6
Mickle_PC <mickle.simakov@gmail.com>
2025-04-30 14:03:32 +0300

    Final version *without duplicates

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -2,47 +2,42 @@ import { FC, useState } from "react";
 import { IData_SnippetNews } from "../../types/types";
 import { Tag, Typography } from 'antd';
 import './card-content.css';
-type Props = {
-  data: IData_SnippetNews
-}
 
-export const CardContent: FC<Props> = ({data}) => {
-  const [symbol, setSymbol] = useState('Show more');
+export const CardContent: FC<{data: IData_SnippetNews}> = ({data}) => {
   const [expanded, setExpanded] = useState(false);
-  const highlights = data.HIGHLIGHTS.map(highlight => highlight.slice(1)).join('; ');
+  const highlights = data.HIGHLIGHTS.map(h => h.slice(1)).join('; ');
 
   return (
     <div className="card-content">
       <Typography.Paragraph
         className="card-content__text"
         ellipsis={{
-          symbol: symbol,
+          symbol: expanded ? 'Hide' : 'Show more',
           rows: 3,
           expandable: 'collapsible',
           expanded,
-          onExpand: (_, info) => {
-            setExpanded(info.expanded);
-            setSymbol(info.expanded ? 'Hide' : 'Show more');
-          },
+          onExpand: (_, info) => setExpanded(info.expanded),
         }}
       >
-        {highlights.split('<kw>').map((part, i) => {
-          if (i === 0) return part;
-          const [keyword, rest] = part.split('</kw>');
-          return (
+        {highlights.split('<kw>').map((part, i) => 
+          i === 0 ? part : (
             <>
-              <span className="highlight-border">{keyword}</span>
-              {rest}
+              <span className="highlight-border">{part.split('</kw>')[0]}</span>
+              {part.split('</kw>')[1]}
             </>
-          );
-        })}
+          )
+        )}
       </Typography.Paragraph>
       <div className="content-tags">
-        {data.KW.map((item, index) => (
-          <Tag key={index} className="content-tags__item">{item.value} <span style={{color: 'white'}}>{item.count}</span></Tag>
+        {data.KW.map((item, i) => (
+          <Tag key={i} className="content-tags__item">
+            {item.value} <span style={{color: 'white'}}>{item.count}</span>
+          </Tag>
         ))}
       </div>
-      <a href={data.URL} target="_blank" rel="noreferrer"><button className="card-content__button">Original Source</button></a>
+      <a href={data.URL} target="_blank" rel="noreferrer">
+        <button className="card-content__button">Original Source</button>
+      </a>
     </div>
   );
 };
--- src/components/CardHeader/CardHeader.tsx
+++ src/components/CardHeader/CardHeader.tsx
@@ -3,52 +3,48 @@ import { IData_SnippetNews } from "../../types/types";
 import dayjs from "dayjs";
 import './card-header.css'
 
-type Props = {
-  data: IData_SnippetNews
-}
-
-type HeaderTagItemProps = {
-  icon: string
-  text: string
-}
+type Props = { data: IData_SnippetNews }
+type HeaderTagItemProps = { icon: string, text: string }
 
 export const CardHeader: FC<Props> = ({data}) => {
   const date = dayjs(data.DP).format('DD MMM YYYY')
+  const [day, rest] = [date.slice(0, 2), date.slice(2)]
 
   return (
     <div className='card-header'>
       <div className="header-info">
-        <span className="header-info__date"><span>{date.slice(0, 2)}</span><span className="text-gray">{date.slice(2)}</span></span>
+        <span className="header-info__date"><span>{day}</span><span className="text-gray">{rest}</span></span>
         <span className="header-info__reach">{Math.round(data.REACH / 1000)}K <span className="text-gray">Reach</span></span>
         <span className="header-info__traffic text-gray">
           Top Traffic:
-          {data.TRAFFIC.map((item) => {
-            return (
-              <div key={item.value}>
-                <span>{item.value}</span>
-                <span style={{color: 'white'}}>{`${Math.round(item.count * 100 / 1)}%`}</span>
-              </div>
-            )
-          })}
+          {data.TRAFFIC.map(item => (
+            <div key={item.value}>
+              <span>{item.value}</span>
+              <span style={{color: 'white'}}>{`${Math.round(item.count * 100)}%`}</span>
+            </div>
+          ))}
         </span>
-        {data.SENT === 'positive' ? <div className="header-info__status">Positive</div> : <div className="header-info__status status-negative">Negative</div>}
+        <div className={`header-info__status ${data.SENT === 'positive' ? '' : 'status-negative'}`}>
+          {data.SENT === 'positive' ? 'Positive' : 'Negative'}
+        </div>
       </div>
       <h2 className="header-title">{data.TI}</h2>
       <div className="header-tags">
-        <HeaderTagItem icon={'/icons/domen.svg'} text={data.DOM} />
-        <HeaderTagItem icon={'/icons/flag.svg'} text={data.CNTR} />
-        <HeaderTagItem icon={'/icons/language.svg'} text={data.LANG.toUpperCase()} />
-        {data.AU.length > 0 ? <HeaderTagItem icon={'/icons/author.svg'} text={data.AU.join(', ')} /> : <HeaderTagItem icon={'/icons/author.svg'} text={'?'} />}
+        <HeaderTagItem icon='/icons/domen.svg' text={data.DOM} />
+        <HeaderTagItem icon='/icons/flag.svg' text={data.CNTR} />
+        <HeaderTagItem icon='/icons/language.svg' text={data.LANG.toUpperCase()} />
+        <HeaderTagItem icon='/icons/author.svg' text={data.AU.length ? data.AU.join(', ') : '?'} />
       </div>
     </div>
   );
 }
 
-const HeaderTagItem: FC<HeaderTagItemProps> = ({icon, text}) => {
-  return (
-    <div className="header-tags__item">
-      <img src={icon} alt={'header-tag-icon'} width={18} height={18}/>
-      {icon.includes('domen') ? <a href={`https://${text}`} target="_blank" rel="noreferrer">{text}</a> : <span>{text}</span>}
-    </div>
-  )
-}
+const HeaderTagItem: FC<HeaderTagItemProps> = ({icon, text}) => (
+  <div className="header-tags__item">
+    <img src={icon} alt='header-tag-icon' width={18} height={18}/>
+    {icon.includes('domen') ? 
+      <a href={`https://${text}`} target="_blank" rel="noreferrer">{text}</a> : 
+      <span>{text}</span>
+    }
+  </div>
+)
