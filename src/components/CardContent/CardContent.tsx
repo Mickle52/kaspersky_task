b70d6467a9e6873951d9d0ee50f740e6ae61d68e
Mickle_PC <mickle.simakov@gmail.com>
2025-04-30 13:39:02 +0300

    Add main content

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import { IData_SnippetNews } from "../../types/types";
-import { Typography } from 'antd';
+import { Tag, Typography } from 'antd';
 import './card-content.css';
 type Props = {
   data: IData_SnippetNews
@@ -17,7 +17,7 @@ export const CardContent: FC<Props> = ({data}) => {
         className="card-content__text"
         ellipsis={{
           symbol: symbol,
-          rows: 2,
+          rows: 3,
           expandable: 'collapsible',
           expanded,
           onExpand: (_, info) => {
@@ -25,19 +25,24 @@ export const CardContent: FC<Props> = ({data}) => {
             setSymbol(info.expanded ? 'Hide' : 'Show more');
           },
         }}
-        aria-label={data.CNTR}
       >
         {highlights.split('<kw>').map((part, i) => {
           if (i === 0) return part;
           const [keyword, rest] = part.split('</kw>');
           return (
             <>
-              <span style={{backgroundColor: 'var(--color-blue)', color: 'white', padding: '2px 4px', borderRadius: '4px'}}>{keyword}</span>
+              <span className="highlight-border">{keyword}</span>
               {rest}
             </>
           );
         })}
       </Typography.Paragraph>
+      <div className="content-tags">
+        {data.KW.map((item, index) => (
+          <Tag key={index} className="content-tags__item">{item.value} <span style={{color: 'white'}}>{item.count}</span></Tag>
+        ))}
+      </div>
+      <a href={data.URL} target="_blank" rel="noreferrer"><button className="card-content__button">Original Source</button></a>
     </div>
   );
 };
